f5522426e9b22d3fa0d4913b9ee3764fda5220ec
leikang <tju.leikang@gmail.com>
2012-05-14 00:41:13 +0800

    little change
    Signed-off-by: leikang <tju.leikang@gmail.com>

--- src/com/nodejs/comic/ClassifyActivity.java
+++ src/com/nodejs/comic/ClassifyActivity.java
@@ -1,12 +1,7 @@
 package com.nodejs.comic;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.os.Bundle;
-
-import com.nodejs.comicapi.API;
-import com.nodejs.comicapi.model.Classify;
 public class ClassifyActivity extends Activity{
 	
 	@Override
